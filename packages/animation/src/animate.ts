938dfa6189af76f071936f28b760bbbe2801a111
Zhang Haocong <kakera@gmail.com>
2025-04-23 22:59:56 +0800

    feat(Animations): Introduce animations

---
+++ packages/animation/src/animate.ts
@@ -0,0 +1,77 @@
+import { Point, RenderObject, RenderShape, Size } from '@canvas-ui/core'
+import { animate as impl, ValueAnimationTransition } from 'motion'
+
+type MotionProps = {
+  x?: number
+  y?: number
+  width?: number
+  height?: number
+  rotation?: number
+}
+
+type AnimatableProps = {
+  offset?: RenderObject['offset']
+  size?: RenderObject['size']
+  rotation?: RenderShape['unstable_rotation']
+}
+
+export function animate(
+  object: RenderObject,
+  props: AnimatableProps,
+  options: ValueAnimationTransition<AnimatableProps> = {},
+) {
+  const { initialProps, targetProps } = buildMotionProps(object, props)
+  const onUpdate = () => {
+    // motion will change initialProps in-place during animation
+    applyMotionProps(object, initialProps)
+    options.onUpdate?.(initialProps)
+  }
+
+  return impl(initialProps, targetProps, {
+    ...options,
+    onUpdate,
+  })
+}
+
+function applyMotionProps(object: RenderObject, props: MotionProps) {
+  let isParentDirty = false
+  if (props.x !== undefined) {
+    object._offset = Point.fromXY(props.x, props.y!)
+    isParentDirty = true
+  }
+  if (props.width !== undefined) {
+    object._size = Size.fromWH(props.width, props.height!)
+    isParentDirty = true
+  }
+  if (isParentDirty) {
+    object.parent?.markPaintDirty()
+  }
+  if (props.rotation !== undefined && object instanceof RenderShape) {
+    object.unstable_rotation = props.rotation
+  }
+}
+
+function buildMotionProps(object: RenderObject, props: AnimatableProps) {
+  const initialProps: MotionProps = {}
+  const targetProps: MotionProps = {}
+  if (props.offset) {
+    initialProps.x = object.offset.x
+    initialProps.y = object.offset.y
+    targetProps.x = props.offset.x
+    targetProps.y = props.offset.y
+  }
+  if (props.size) {
+    initialProps.width = object.size.width
+    initialProps.height = object.size.height
+    targetProps.width = object.size.width
+    targetProps.height = object.size.height
+  }
+  if (typeof props.rotation === 'number' && object instanceof RenderShape) {
+    initialProps.rotation = object.unstable_rotation
+    targetProps.rotation = props.rotation
+  }
+  return {
+    initialProps,
+    targetProps,
+  }
+}
---
+++ packages/storybook/stories/animation-basic.stories.tsx
@@ -0,0 +1,80 @@
+import { DebugFlags, Point, Rect, RenderPath, RenderView, Size } from '@canvas-ui/core'
+import { Canvas, Path, Text, View } from '@canvas-ui/react'
+import { StoryObj } from '@storybook/react'
+import { animate } from '@canvas-ui/animation'
+import { spring } from 'motion'
+import React from 'react'
+
+const path = `M266.24 420.0448a30.8736 30.8736 0 0 1 52.736-21.8624L512 591.0016l192.9728-192.8192a30.8736 30.8736 0 1 1 43.7248 43.7248l-214.8352 214.6304a31.0272 31.0272 0 0 1-43.776 0L275.3024 441.9072a30.8736 30.8736 0 0 1-9.0624-21.8624z`
+
+DebugFlags.set(
+  0
+  | DebugFlags.NodeBounds
+  | DebugFlags.LayerBounds
+  | DebugFlags.RasterCacheWaterMark
+  | DebugFlags.PathBounds
+)
+
+export const AnimationBasic: StoryObj<React.FC> = () => {
+
+  const viewRef = React.useRef<RenderView>(null)
+
+  const handleClickLeft = () => {
+    move(-100)
+  }
+
+  const handleClickRight = () => {
+    move(100)
+  }
+
+  const move = (delta: number) => {
+
+    animate(viewRef.current!, {
+      offset: Point.add(viewRef.current!.offset, Point.fromXY(delta, 0)),
+    }, {
+      duration: 0.8,
+      type: spring,
+    })
+
+    const pathEl = viewRef.current?.firstChild as RenderPath
+    pathEl.unstable_rotation = 0
+    animate(pathEl, {
+      rotation: 360 * Math.sign(delta),
+    }, {
+      duration: 1.333,
+      type: spring,
+    })
+  }
+
+  return (
+    <div style={{ height: '100px' }}>
+      <div>
+        <button onClick={handleClickLeft}>move left</button>
+        <button onClick={handleClickRight}>move right</button>
+      </div>
+      <Canvas>
+        <View ref={viewRef} size={Size.fromWH(200, 200)}>
+          <Path
+            fill={'#ff0000'}
+            path={path}
+            style={{ left: 0, top: 0, width: 100, height: 100 }}
+            pathBounds={Rect.fromLTWH(0, 0, 1024, 1024)}
+            unstable_rotation={0}
+            unstable_transformOrigin={Point.fromXY(0.5, 0.5)}
+          />
+          <Text>
+            Monkey 🐒
+          </Text>
+        </View>
+      </Canvas>
+    </div>
+  )
+}
+
+AnimationBasic.storyName = 'AnimationBasic'
+
+export default {
+  title: 'animation',
+  component: AnimationBasic,
+  decorators: [(Story: React.ComponentType) => <div style={{ backgroundColor: '#efefef', width: '100%', height: '100vh' }}><Story /></div>],
+}
