fcb6ace1a5077f9ee9f0d6f892b02b9483eff932
Zhang Haocong <kakera@gmail.com>
2025-06-13 13:32:16 +0800

    Init docusaurus

--- tools/dev.sh
+++ tools/dev.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
 pnpm distclean \
- && pnpm --filter "@canvas-ui/*" run --stream build \
- && pnpm --filter "@canvas-ui/*" run --parallel dev
+ && pnpm --filter "@canvas-ui/*" --filter "!@canvas-ui/docs" run --stream build \
+ && pnpm --filter "@canvas-ui/*" --filter "!@canvas-ui/docs" run --parallel dev
